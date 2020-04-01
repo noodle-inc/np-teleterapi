24753d2bbfe5484e858324e278d209f4dff4df35
Kerem Dinçer <b.keremdincer@gmail.com>
2020-04-02 00:15:24 +0300

    Mustache kaldırıldı. Pug eklendi.
    Co-authored-by: kocsefa <koc.sefa@hotmail.com>

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 const express = require('express')
-const mustacheExpress = require('mustache-express')
 const app = express()
 const port = 11999
 
 const meetingsRouter = require('./routers/meetings')
 
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.set('views', `${__dirname}/views`)
-app.set('view engine', 'mustache')
-app.engine('mustache', mustacheExpress())
+app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.render('index', { foo: 'bar' }))
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
-app.use('/meetings', meetingsRouter)
+app.use('/api/meetings', meetingsRouter)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
